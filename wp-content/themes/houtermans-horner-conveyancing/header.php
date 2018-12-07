50a2dee95b2c96cf06182dcca140a03d9b61f2ee
scottys88 <scott_schubert@hotmail.com.au>
2018-12-07 22:07:49 +1030

    Up[date header tpo include meta des

--- wp-content/themes/houtermans-horner-conveyancing/header.php
+++ wp-content/themes/houtermans-horner-conveyancing/header.php
@@ -18,7 +18,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="<?php the_sub_field('meta_description'); ?>">
+    <meta name="description" content="<?php the_field('meta_description'); ?>">
     <meta name="author" content="">
 	
 	<?php wp_head(); ?>
